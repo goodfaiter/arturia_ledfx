92f97f3232c589c98a2855214a27d2f571bcaa57
Valentin Yuryev <goodfaiter@gmail.com>
2024-09-02 17:58:07 +0200

    added missinmg imports

--- src/runner.py
+++ src/runner.py
@@ -2,7 +2,7 @@ import mido
 from typing import Dict, List
 import sys
 import yaml
-from .api_calls import ApiCall, AdjustBrightness, OneShot, SwitchToEffect, SwitchToEffectAndPreset
+from .api_calls import ApiCall, AdjustBrightness, OneShot, Ripple, SwitchToEffect, SwitchToEffectAndPreset, Wave
 from .midi_inputs import MidiInput
 
 
